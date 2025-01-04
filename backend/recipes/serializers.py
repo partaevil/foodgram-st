19193d8e00e5093841edd0b339d63e5964b054e7
partaevil <you@example.com>
2025-01-04 19:12:29 +0300

    setup recipe logic

---
+++ backend/recipes/serializers.py
@@ -0,0 +1,66 @@
+from rest_framework import serializers
+from core.models import Recipe, RecipeIngredient
+from users.serializers import UserSerializer
+from core.serializers import Base64ImageField
+
+class IngredientInRecipeSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(source='ingredient.id')
+    name = serializers.CharField(source='ingredient.name')
+    measurement_unit = serializers.CharField(source='ingredient.measurement_unit')
+
+    class Meta:
+        model = RecipeIngredient
+        fields = ('id', 'name', 'measurement_unit', 'amount')
+
+class RecipeSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    ingredients = IngredientInRecipeSerializer(source='recipe_ingredients', 
+                                             many=True, read_only=True)
+    is_favorited = serializers.SerializerMethodField()
+    is_in_shopping_cart = serializers.SerializerMethodField()
+    image = Base64ImageField()
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'author', 'ingredients', 'is_favorited', 
+                 'is_in_shopping_cart', 'name', 'image', 'text', 'cooking_time')
+
+    def get_is_favorited(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            return obj.favorited_by.filter(user=request.user).exists()
+        return False
+
+    def get_is_in_shopping_cart(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            return obj.in_carts_of.filter(user=request.user).exists()
+        return False
+
+    def create(self, validated_data):
+        ingredients_data = self.context.get('ingredients', [])
+        recipe = Recipe.objects.create(
+            author=self.context['request'].user,
+            **validated_data
+        )
+        
+        for ingredient in ingredients_data:
+            RecipeIngredient.objects.create(
+                recipe=recipe,
+                ingredient_id=ingredient['id'],
+                amount=ingredient['amount']
+            )
+        return recipe
+
+    def update(self, instance, validated_data):
+        ingredients_data = self.context.get('ingredients', [])
+        instance.recipe_ingredients.all().delete()
+        
+        for ingredient in ingredients_data:
+            RecipeIngredient.objects.create(
+                recipe=instance,
+                ingredient_id=ingredient['id'],
+                amount=ingredient['amount']
+            )
+            
+        return super().update(instance, validated_data)
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from core.models import Recipe 
+from .serializers import RecipeSerializer
+from django.shortcuts import get_object_or_404
+import hashlib
 
-# Create your views here.
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        if self.request.method in ['POST', 'PATCH']:
+            context['ingredients'] = self.request.data.get('ingredients', [])
+        return context
+
+    @action(detail=True, methods=['get'])
+    def get_link(self, request, pk=None):
+        recipe = self.get_object()
+        # Create a simple hash of the recipe ID
+        hash_object = hashlib.md5(str(recipe.id).encode())
+        short_hash = hash_object.hexdigest()[:3]
+        short_link = f"https://localhost/s/{short_hash}"
+        return Response({'short-link': short_link})
