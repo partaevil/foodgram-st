8c707e509f6a210df1428caa5d54ed6b755e2fed
partaevil <you@example.com>
2025-01-04 14:44:26 +0300

    migration

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
