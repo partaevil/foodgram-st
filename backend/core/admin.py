de152bbe9e0c22d0dc98ceedfc6b4767e9170492
partaevil <you@example.com>
2025-01-05 15:10:31 +0300

    refactor admin panel

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -5,9 +5,17 @@ from .models import (
     UserProfile, Ingredient, Recipe, RecipeIngredient,
     Favorite, ShoppingCart, Subscription
 )
+from django.contrib.auth import get_user_model
 
-# Register UserProfile
-admin.site.register(UserProfile)
+User = get_user_model()
+
+admin.site.unregister(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('username', 'email')
+    list_filter = ('is_staff', 'is_active')
 
 class IngredientResource(ModelResource):
     class Meta:
@@ -22,29 +30,44 @@ class IngredientResource(ModelResource):
 class IngredientAdmin(ImportExportModelAdmin):
     resource_class = IngredientResource
     list_display = ('name', 'measurement_unit')
-    search_fields = ('name', 'measurement_unit')
+    search_fields = ('name',)
 
-# Customize and register Recipe
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1  
+    autocomplete_fields = ('ingredient',)  
+
+@admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author', 'cooking_time')
+    list_display = ('name', 'author', 'cooking_time', 'favorites_count')
     list_filter = ('author', 'cooking_time')
-    search_fields = ('name', 'text')
+    search_fields = ('name', 'author__username')
+    inlines = [RecipeIngredientInline] 
 
-admin.site.register(Recipe, RecipeAdmin)
+    def favorites_count(self, obj):
+        return obj.favorited_by.count()
+    favorites_count.short_description = 'Добавлений в избранное'
 
-# Customize and register RecipeIngredient
+@admin.register(RecipeIngredient)
 class RecipeIngredientAdmin(admin.ModelAdmin):
     list_display = ('recipe', 'ingredient', 'amount')
     list_filter = ('recipe', 'ingredient')
     search_fields = ('recipe__name', 'ingredient__name')
 
-admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe')
+    list_filter = ('user', 'recipe')
+    search_fields = ('user__username', 'recipe__name')
 
-# Register Favorite
-admin.site.register(Favorite)
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe')
+    list_filter = ('user', 'recipe')
+    search_fields = ('user__username', 'recipe__name')
 
-# Register ShoppingCart
-admin.site.register(ShoppingCart)
-
-# Register Subscription
-admin.site.register(Subscription)
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author', 'recipes_count')
+    list_filter = ('user', 'author')
+    search_fields = ('user__username', 'author__username')
