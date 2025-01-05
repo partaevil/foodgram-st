fc387aa7ecbc4651c712047d9e6c0ffc92255b58
partaevil <you@example.com>
2025-01-05 12:33:00 +0300

    update recipe urls so they have basenames

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from .views import RecipeViewSet, IngredientViewSet
 
 router = DefaultRouter()
-router.register('recipes', RecipeViewSet)
-router.register('ingredients', IngredientViewSet)
+router.register('recipes', RecipeViewSet, basename='recipe')
+router.register('ingredients', IngredientViewSet, basename='ingredient')
 
 urlpatterns = [
     path('', include(router.urls)),
