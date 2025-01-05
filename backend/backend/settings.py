28fc18a442ff1272b151efc950338c44e56da473
partaevil <you@example.com>
2025-01-05 13:11:06 +0300

    Add media support to django

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -103,6 +103,8 @@ DATABASES = {
     }
 }
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
