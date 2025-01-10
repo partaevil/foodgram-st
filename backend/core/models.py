ac596040086326d17507ec06e1c8f6db543687b4
partaevil <you@example.com>
2025-01-10 14:45:28 +0300

    change to ' where possible in models

--- backend/core/models.py
+++ backend/core/models.py
@@ -11,18 +11,18 @@ class UserProfile(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='profiles',
-        verbose_name="User"
+        verbose_name='User'
     )
     avatar = models.ImageField(
         upload_to='avatars/',
         blank=True,
         null=True,
-        verbose_name="Avatar"
+        verbose_name='Avatar'
     )
 
     class Meta:
-        verbose_name = "User Profile"
-        verbose_name_plural = "User Profiles"
+        verbose_name = 'User Profile'
+        verbose_name_plural = 'User Profiles'
 
     def __str__(self):
         return f"{self.user.username}'s profile"
@@ -31,20 +31,20 @@ class UserProfile(models.Model):
 class Ingredient(models.Model):
     name = models.CharField(
         max_length=128,
-        verbose_name="Name"
+        verbose_name='Name'
     )
     measurement_unit = models.CharField(
         max_length=64,
-        verbose_name="Measurement Unit"
+        verbose_name='Measurement Unit'
     )
 
     class Meta:
-        verbose_name = "Ingredient"
-        verbose_name_plural = "Ingredients"
+        verbose_name = 'Ingredient'
+        verbose_name_plural = 'Ingredients'
         ordering = ['name']
 
     def __str__(self):
-        return f"{self.name} ({self.measurement_unit})"
+        return f'{self.name} ({self.measurement_unit})'
 
 
 class Recipe(models.Model):
@@ -52,31 +52,31 @@ class Recipe(models.Model):
         User,
         related_name='recipes',
         on_delete=models.CASCADE,
-        verbose_name="Author"
+        verbose_name='Author'
     )
     name = models.CharField(
         max_length=256,
-        verbose_name="Name"
+        verbose_name='Name'
     )
     image = models.ImageField(
         upload_to='recipes/images/',
-        verbose_name="Image"
+        verbose_name='Image'
     )
     text = models.TextField(
-        verbose_name="Description"
+        verbose_name='Description'
     )
     cooking_time = models.IntegerField(
         validators=[MinValueValidator(1)],
-        verbose_name="Cooking Time (minutes)"
+        verbose_name='Cooking Time (minutes)'
     )
     date_published = models.DateTimeField(
         default=now,
-        verbose_name="Date Published"
+        verbose_name='Date Published'
     )
 
     class Meta:
-        verbose_name = "Recipe"
-        verbose_name_plural = "Recipes"
+        verbose_name = 'Recipe'
+        verbose_name_plural = 'Recipes'
         ordering = ['-date_published']
 
     def __str__(self):
@@ -88,13 +88,13 @@ class RecipeIngredient(models.Model):
         Recipe,
         related_name='recipe_ingredients',
         on_delete=models.CASCADE,
-        verbose_name="Recipe"
+        verbose_name='Recipe'
     )
     ingredient = models.ForeignKey(
         Ingredient,
         related_name='recipe_ingredients',
         on_delete=models.CASCADE,
-        verbose_name="Ingredient"
+        verbose_name='Ingredient'
     )
     amount = models.IntegerField(
         validators=[MinValueValidator(1)],
@@ -115,22 +115,22 @@ class Favorite(models.Model):
         User,
         related_name='favorites',
         on_delete=models.CASCADE,
-        verbose_name="User"
+        verbose_name='User'
     )
     recipe = models.ForeignKey(
         Recipe,
         related_name='favorited_by_users',  # do not change to favorites
         on_delete=models.CASCADE,
-        verbose_name="Recipe"
+        verbose_name='Recipe'
     )
 
     class Meta:
         unique_together = ('user', 'recipe')
-        verbose_name = "Favorite"
-        verbose_name_plural = "Favorites"
+        verbose_name = 'Favorite'
+        verbose_name_plural = 'Favorites'
 
     def __str__(self):
-        return f"{self.user.username} favors {self.recipe.name}"
+        return f'{self.user.username} favors {self.recipe.name}'
 
 
 class ShoppingCart(models.Model):
@@ -138,22 +138,22 @@ class ShoppingCart(models.Model):
         User,
         related_name='shopping_carts',
         on_delete=models.CASCADE,
-        verbose_name="User"
+        verbose_name='User'
     )
     recipe = models.ForeignKey(
         Recipe,
         related_name='in_carts_of_users',
         on_delete=models.CASCADE,
-        verbose_name="Recipe"
+        verbose_name='Recipe'
     )
 
     class Meta:
         unique_together = ('user', 'recipe')
-        verbose_name = "Shopping Cart"
-        verbose_name_plural = "Shopping Carts"
+        verbose_name = 'Shopping Cart'
+        verbose_name_plural = 'Shopping Carts'
 
     def __str__(self):
-        return f"{self.user.username} has {self.recipe.name} in cart"
+        return f'{self.user.username} has {self.recipe.name} in cart'
 
 
 class Subscription(models.Model):
@@ -161,23 +161,23 @@ class Subscription(models.Model):
         User,
         related_name='subscriptions',
         on_delete=models.CASCADE,
-        verbose_name="User"
+        verbose_name='User'
     )
     author = models.ForeignKey(
         User,
         related_name='subscribers',
         on_delete=models.CASCADE,
-        verbose_name="Author"
+        verbose_name='Author'
     )
     recipes_count = models.IntegerField(
         default=0,
-        verbose_name="Number of Recipes"
+        verbose_name='Number of Recipes'
     )
 
     class Meta:
         unique_together = ('user', 'author')
-        verbose_name = "Subscription"
-        verbose_name_plural = "Subscriptions"
+        verbose_name = 'Subscription'
+        verbose_name_plural = 'Subscriptions'
 
     def __str__(self):
-        return f"{self.user.username} subscribes to {self.author.username}"
+        return f'{self.user.username} subscribes to {self.author.username}'
